503ea20f80b38da80b2cf125d913f3c399789913
Thomas Dybdahl Ahle <thomas@ahle.dk>
2016-07-29 20:42:45 +0200

    Added test for bad move in issue 34

--- test.py
+++ test.py
@@ -37,13 +37,28 @@ class TestValueFunction(unittest.TestCase):
             self.assertEqual(pos.score, score,
                     ' '.join(pos.board) + repr(pos))
 
+    def test_san(self):
+        pgn_file = os.path.join(os.path.dirname(__file__), 'tests/pgns.pgn')
+        for line in open(pgn_file):
+            msans = [msan for i, msan in enumerate(line.split()[:-1]) if i%3!=0]
+            pos = xboard.parseFEN(xboard.FEN_INITIAL)
+            for i, msan in enumerate(msans):
+                move = parseSAN(pos, i%2, msan)
+                if re.search('=[BNR]', msan):
+                    # Sunfish doesn't support underpromotion
+                    break
+                msan_back = renderSAN(pos, i%2, move)
+                self.assertEqual(msan_back, msan,
+                        "Sunfish didn't correctly reproduce the SAN move")
+                pos = pos.move(move)
+
 ###############################################################################
 # Playing test
 ###############################################################################
 
 def selfplay(maxn=200):
     """ Start a game sunfish vs. sunfish """
-    pos = xboard.parseFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+    pos = xboard.parseFEN(xboard.FEN_INITIAL)
     for d in range(200):
         # Always print the board from the same direction
         board = pos.board if d % 2 == 0 else pos.rotate().board
@@ -67,7 +82,7 @@ def play(version1_version2_maxn_rand):
     version1, version2, maxn, rand = version1_version2_maxn_rand
     fish1 = importlib.import_module(version1)
     fish2 = importlib.import_module(version2)
-    pos = xboard.parseFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+    pos = xboard.parseFEN(xboard.FEN_INITIAL)
     old = None
     tdelta = 0
     for d in range(200):
@@ -113,11 +128,9 @@ def testxboard(python='python3'):
        universal_newlines=True)
 
     def waitFor(regex):
-        # print('waiting for', regex)
         with timeout(20, '%s was never encountered'%regex):
             while True:
                 line = fish.stdout.readline()
-                # print("Saw lines", line)
                 if re.search(regex, line):
                     return
 
@@ -241,10 +254,20 @@ def quickmate(f, min_depth=1, draw=False):
 # Best move test
 ###############################################################################
 
-def renderSAN(pos, move):
-    # TODO: How do we simply make this work for black as well?
+def gen_legal_moves(pos):
+    ''' pos.gen_moves(), but without those that leaves us in check '''
+    for move in pos.gen_moves():
+        pos1 = pos.move(move)
+        if not any(pos1.board[j] == 'k' or j == pos1.kp for i,j in pos1.gen_moves()):
+            yield move
+
+def renderSAN(pos, color, move):
+    ''' Assumes board is rotated to position of current player '''
     i, j = move
     csrc, cdst = sunfish.render(i), sunfish.render(j)
+    # Rotate flor black
+    if color == xboard.BLACK:
+        csrc, cdst = sunfish.render(119-i), sunfish.render(119-j)
     # Check
     pos1 = pos.move(move)
     cankill = lambda p: any(p.board[b]=='k' for a,b in p.gen_moves())
@@ -254,24 +277,32 @@ def renderSAN(pos, move):
         if all(cankill(pos1.move(move1)) for move1 in pos1.gen_moves()):
             check = '#'
     # Castling
-    if pos.board[i] == 'K' and csrc == 'e1' and cdst in ('c1','g1'):
-        if cdst == 'c1':
+    if pos.board[i] == 'K' and abs(i-j) == 2:
+        if color == xboard.WHITE and j > i or color == xboard.BLACK and j < i:
+            return 'O-O' + check
+        else:
             return 'O-O-O' + check
-        return 'O-O' + check
     # Pawn moves
     if pos.board[i] == 'P':
-        pro = '=Q' if cdst[1] == '8' else ''
+        pro = '=Q' if sunfish.A8 <= j <= sunfish.H8 else ''
         cap = csrc[0] + 'x' if pos.board[j] != '.' or j == pos.ep else ''
         return cap + cdst + pro + check
+    # Figure out what files and ranks we need to include
+    srcs = [a for a,b in gen_legal_moves(pos) if pos.board[a] == pos.board[i] and b == j]
+    srcs_file = [a for a in srcs if (a - sunfish.A1) % 10 == (i - sunfish.A1) % 10]
+    srcs_rank = [a for a in srcs if (a - sunfish.A1) // 10 == (i - sunfish.A1) // 10]
+    assert len(srcs) > 0
+    if len(srcs) == 1: src = ''
+    elif len(srcs_file) == 1: src = csrc[0]
+    elif len(srcs_rank) == 1: src = csrc[1]
+    else: src = csrc
     # Normal moves
     p = pos.board[i]
-    srcs = [a for a,b in pos.gen_moves() if pos.board[a] == p and b == j]
-    # TODO: We can often get away with just sending the rank or file here.
-    src = csrc if len(srcs) > 1 else ''
     cap = 'x' if pos.board[j] != '.' else ''
     return p + src + cap + cdst + check
 
 def parseSAN(pos, color, msan):
+    ''' Assumes board is rotated to position of current player '''
     # Normal moves
     normal = re.match('([KQRBN])([a-h])?([1-8])?x?([a-h][1-8])', msan)
     if normal:
@@ -283,18 +314,20 @@ def parseSAN(pos, color, msan):
         p, (fil, dst) = 'P', pawn.groups()
         src = (fil or '[a-h]')+'[1-8]'
     # Castling
-    if msan == "O-O-O":
-        p, src, dst = 'K', 'e1|d1', 'c1|f1'
-    if msan == "O-O":
-        p, src, dst = 'K', 'e1|d1', 'g1|b1'
+    if re.match(msan, "O-O-O[+#]?"):
+        p, src, dst = 'K', 'e[18]', 'c[18]'
+    if re.match(msan, "O-O[+#]?"):
+        p, src, dst = 'K', 'e[18]', 'g[18]'
     # Find possible match
-    for i, j in pos.gen_moves():
-        # TODO: Maybe check for check here?
-        csrc, cdst = sunfish.render(i), sunfish.render(j)
-        if pos.board[i] == p and re.match(dst, cdst) and re.match(src, csrc):
+    for i, j in gen_legal_moves(pos):
+        if color == xboard.WHITE:
+            csrc, cdst = sunfish.render(i), sunfish.render(j)
+        else: csrc, cdst = sunfish.render(119-i), sunfish.render(119-j)
+        if pos.board[i] == p and re.match(dst,cdst) and re.match(src,csrc):
             return (i, j)
+    assert False
 
-def parseEPD(epd):
+def parseEPD(epd, opt_dict=False):
     epd = epd.strip('\n ;').replace('"','')
     parts = epd.split(maxsplit=6)
     opt_part = ''
@@ -306,42 +339,49 @@ def parseEPD(epd):
         fen = ' '.join(parts[:4]) + ' 0 1'
         opt_part = ' '.join(parts[4:])
     # EPD operations may either be <opcode> or (<opcode> <operand>)
-    opts = [p.split(maxsplit=1) for p in opt_part.split(';')]
+    opts = opt_part.split(';')
+    if opt_dict:
+        opts = dict(p.split(maxsplit=1) for p in opts)
     return fen, opts
 
 def findbest(f, times):
     print('Calibrating search speed...')
-    pos = xboard.parseFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-    CAL_NODES = 10000
+    pos = xboard.parseFEN(xboard.FEN_INITIAL)
+    CAL_NODES = 100000
     start = time.time()
     _ = sunfish.search(pos, CAL_NODES)
-    factor = CAL_NODES/(time.time()-start)
+    factor = sunfish.nodes/(time.time()-start)
 
     print('Running benchmark with %.1f nodes per second...' % factor)
+    print('Printing best move after seconds', times)
     print('-'*60)
     totalpoints = 0
     totaltests = 0
     for line in f:
-        fen, opts = parseEPD(line)
+        fen, opts = parseEPD(line, opt_dict=True)
+        if type(opts) != dict or ('am' not in opts and 'bm' not in opts):
+            print("Line didn't have am/bm in opts", line, opts)
+            continue
         pos = xboard.parseFEN(fen)
-        color = 0 if fen.split()[1] == 'w' else 1
+        color = xboard.WHITE if fen.split()[1] == 'w' else xboard.BLACK
         # am -> avoid move; bm -> best move
         am = parseSAN(pos,color,opts['am']) if 'am' in opts else None
         bm = parseSAN(pos,color,opts['bm']) if 'bm' in opts else None
+        print('Looking for am/bm', opts.get('am'), opts.get('bm'))
         points = 0
         print(opts.get('id','unnamed'), end=' ', flush=True)
         for t in times:
             move, _ = sunfish.search(pos, factor*t)
-            mark = renderSAN(pos, move)
+            mark = renderSAN(pos,color,move)
             if am and move != am or bm and move == bm:
                 mark += '(1)'
                 points += 1
-                totalpoints += 1
             else:
                 mark += '(0)'
             print(mark, end=' ', flush=True)
-            totaltests + 1
+            totaltests += 1
         print(points)
+        totalpoints += points
     print('-'*60)
     print('Total Points: %d/%d', totalpoints, totaltests)
 
