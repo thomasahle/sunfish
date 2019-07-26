3f904c2a68f0a6431e7afdfc53802c6a005d1229
Recursing <buonanno.lorenzo@gmail.com>
2019-07-26 18:59:29 +0200

    Clearer formatting

--- sunfish.py
+++ sunfish.py
@@ -10,7 +10,8 @@ from collections import OrderedDict, namedtuple
 # Piece-Square tables. Tune these to change sunfish's behaviour
 ###############################################################################
 
-piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
+piece = {"P": 100, "N": 280, "B": 320, "R": 479, "Q": 929, "K": 60000}
+
 pst = {
     'P': (   0,   0,   0,   0,   0,   0,   0,   0,
             78,  83,  86,  73, 102,  82,  85,  90,
@@ -63,9 +64,9 @@ pst = {
 }
 # Pad tables and join piece and pst dictionaries
 for k, table in pst.items():
-    padrow = lambda row: (0,) + tuple(x+piece[k] for x in row) + (0,)
-    pst[k] = sum((padrow(table[i*8:i*8+8]) for i in range(8)), ())
-    pst[k] = (0,)*20 + pst[k] + (0,)*20
+    padrow = lambda row: (0,) + tuple(x + piece[k] for x in row) + (0,)
+    pst[k] = sum((padrow(table[i * 8 : i * 8 + 8]) for i in range(8)), ())
+    pst[k] = (0,) * 20 + pst[k] + (0,) * 20
 
 ###############################################################################
 # Global constants
@@ -75,38 +76,38 @@ for k, table in pst.items():
 # fast detection of moves that don't stay within the board.
 A1, H1, A8, H8 = 91, 98, 21, 28
 initial = (
-    '         \n'  #   0 -  9
-    '         \n'  #  10 - 19
-    ' rnbqkbnr\n'  #  20 - 29
-    ' pppppppp\n'  #  30 - 39
-    ' ........\n'  #  40 - 49
-    ' ........\n'  #  50 - 59
-    ' ........\n'  #  60 - 69
-    ' ........\n'  #  70 - 79
-    ' PPPPPPPP\n'  #  80 - 89
-    ' RNBQKBNR\n'  #  90 - 99
-    '         \n'  # 100 -109
-    '         \n'  # 110 -119
+    "         \n"  #   0 -  9
+    "         \n"  #  10 - 19
+    " rnbqkbnr\n"  #  20 - 29
+    " pppppppp\n"  #  30 - 39
+    " ........\n"  #  40 - 49
+    " ........\n"  #  50 - 59
+    " ........\n"  #  60 - 69
+    " ........\n"  #  70 - 79
+    " PPPPPPPP\n"  #  80 - 89
+    " RNBQKBNR\n"  #  90 - 99
+    "         \n"  # 100 -109
+    "         \n"  # 110 -119
 )
 
 # Lists of possible moves for each piece type.
 N, E, S, W = -10, 1, 10, -1
 directions = {
-    'P': (N, N+N, N+W, N+E),
-    'N': (N+N+E, E+N+E, E+S+E, S+S+E, S+S+W, W+S+W, W+N+W, N+N+W),
-    'B': (N+E, S+E, S+W, N+W),
-    'R': (N, E, S, W),
-    'Q': (N, E, S, W, N+E, S+E, S+W, N+W),
-    'K': (N, E, S, W, N+E, S+E, S+W, N+W)
+    "P": (N, N + N, N + W, N + E),
+    "N": (N + N + E, E + N + E, E + S + E, S + S + E, S + S + W, W + S + W, W + N + W, N + N + W),
+    "B": (N + E, S + E, S + W, N + W),
+    "R": (N, E, S, W),
+    "Q": (N, E, S, W, N + E, S + E, S + W, N + W),
+    "K": (N, E, S, W, N + E, S + E, S + W, N + W),
 }
 
-# Mate value must be greater than 8*queen + 2*(rook+knight+bishop)
+# Mate value must be greater than 8 * queen + 2 * (rook + knight + bishop)
 # King value is set to twice this value such that if the opponent is
 # 8 queens up, but we got the king, we still exceed MATE_VALUE.
 # When a MATE is detected, we'll set the score to MATE_UPPER - plies to get there
 # E.g. Mate in 3 will be MATE_UPPER - 6
-MATE_LOWER = piece['K'] - 10*piece['Q']
-MATE_UPPER = piece['K'] + 10*piece['Q']
+MATE_LOWER = piece["K"] - 10 * piece["Q"]
+MATE_UPPER = piece["K"] + 10 * piece["Q"]
 
 # The table size is the maximum number of elements in the transposition table.
 TABLE_SIZE = 1e8
@@ -120,7 +121,7 @@ EVAL_ROUGHNESS = 20
 # Chess logic
 ###############################################################################
 
-class Position(namedtuple('Position', 'board score wc bc ep kp')):
+class Position(namedtuple("Position", "board score wc bc ep kp")):
     """ A state of a chess game
     board -- a 120 char representation of the board
     score -- the board evaluation
@@ -137,31 +138,31 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
         for i, p in enumerate(self.board):
             if not p.isupper(): continue
             for d in directions[p]:
-                for j in count(i+d, d):
+                for j in count(i + d, d):
                     q = self.board[j]
                     # Stay inside the board, and off friendly pieces
                     if q.isspace() or q.isupper(): break
                     # Pawn move, double move and capture
-                    if p == 'P' and d in (N, N+N) and q != '.': break
-                    if p == 'P' and d == N+N and (i < A1+N or self.board[i+N] != '.'): break
-                    if p == 'P' and d in (N+W, N+E) and q == '.' and j not in (self.ep, self.kp): break
+                    if p == "P" and d in (N, N + N) and q != ".": break
+                    if p == "P" and d == N + N and (i < A1 + N or self.board[i + N] != "."): break
+                    if p == "P" and d in (N + W, N + E) and q == "." and j not in (self.ep, self.kp): break
                     # Move it
                     yield (i, j)
                     # Stop crawlers from sliding, and sliding after captures
-                    if p in 'PNK' or q.islower(): break
+                    if p in "PNK" or q.islower(): break
                     # Castling, by sliding the rook next to the king
-                    if i == A1 and self.board[j+E] == 'K' and self.wc[0]: yield (j+E, j+W)
-                    if i == H1 and self.board[j+W] == 'K' and self.wc[1]: yield (j+W, j+E)
+                    if i == A1 and self.board[j + E] == "K" and self.wc[0]: yield (j + E, j + W)
+                    if i == H1 and self.board[j + W] == "K" and self.wc[1]: yield (j + W, j + E)
 
     def rotate(self):
-        ''' Rotates the board, preserving enpassant '''
+        """ Rotates the board, preserving enpassant """
         return Position(
             self.board[::-1].swapcase(), -self.score, self.bc, self.wc,
-            119-self.ep if self.ep else 0,
-            119-self.kp if self.kp else 0)
+            119 - self.ep if self.ep else 0,
+            119 - self.kp if self.kp else 0)
 
     def nullmove(self):
-        ''' Like rotate, but clears ep and kp '''
+        """ Like rotate, but clears ep and kp """
         return Position(
             self.board[::-1].swapcase(), -self.score,
             self.bc, self.wc, 0, 0)
@@ -169,34 +170,34 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
     def move(self, move):
         i, j = move
         p, q = self.board[i], self.board[j]
-        put = lambda board, i, p: board[:i] + p + board[i+1:]
+        put = lambda board, i, p: board[:i] + p + board[i + 1:]
         # Copy variables and reset ep and kp
         board = self.board
         wc, bc, ep, kp = self.wc, self.bc, 0, 0
         score = self.score + self.value(move)
         # Actual move
         board = put(board, j, board[i])
-        board = put(board, i, '.')
+        board = put(board, i, ".")
         # Castling rights, we move the rook or capture the opponent's
         if i == A1: wc = (False, wc[1])
         if i == H1: wc = (wc[0], False)
         if j == A8: bc = (bc[0], False)
         if j == H8: bc = (False, bc[1])
         # Castling
-        if p == 'K':
+        if p == "K":
             wc = (False, False)
-            if abs(j-i) == 2:
-                kp = (i+j)//2
-                board = put(board, A1 if j < i else H1, '.')
-                board = put(board, kp, 'R')
+            if abs(j - i) == 2:
+                kp = (i + j) // 2
+                board = put(board, A1 if j < i else H1, ".")
+                board = put(board, kp, "R")
         # Pawn promotion, double move and en passant capture
-        if p == 'P':
+        if p == "P":
             if A8 <= j <= H8:
-                board = put(board, j, 'Q')
-            if j - i == 2*N:
+                board = put(board, j, "Q")
+            if j - i == 2 * N:
                 ep = i + N
             if j == self.ep:
-                board = put(board, j+S, '.')
+                board = put(board, j + S, ".")
         # We rotate the returned position, so it's ready for the next player
         return Position(board, score, wc, bc, ep, kp).rotate()
 
@@ -207,20 +208,20 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
         score = pst[p][j] - pst[p][i]
         # Capture
         if q.islower():
-            score += pst[q.upper()][119-j]
+            score += pst[q.upper()][119 - j]
         # Castling check detection
-        if abs(j-self.kp) < 2:
-            score += pst['K'][119-j]
+        if abs(j - self.kp) < 2:
+            score += pst["K"][119 - j]
         # Castling
-        if p == 'K' and abs(i-j) == 2:
-            score += pst['R'][(i+j)//2]
-            score -= pst['R'][A1 if j < i else H1]
+        if p == "K" and abs(i - j) == 2:
+            score += pst["R"][(i + j) // 2]
+            score -= pst["R"][A1 if j < i else H1]
         # Special pawn stuff
-        if p == 'P':
+        if p == "P":
             if A8 <= j <= H8:
-                score += pst['Q'][j] - pst['P'][j]
+                score += pst["Q"][j] - pst["P"][j]
             if j == self.ep:
-                score += pst['P'][119-(j+S)]
+                score += pst["P"][119 - (j + S)]
         return score
 
 ###############################################################################
@@ -228,12 +229,12 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
 ###############################################################################
 
 # lower <= s(pos) <= upper
-Entry = namedtuple('Entry', 'lower upper')
+Entry = namedtuple("Entry", "lower upper")
 
 # The normal OrderedDict doesn't update the position of a key in the list,
 # when the value is changed.
 class LRUCache:
-    '''Store items in the order the keys were last added'''
+    """Store items in the order the keys were last added"""
     def __init__(self, size):
         self.od = OrderedDict()
         self.size = size
@@ -262,7 +263,9 @@ class Searcher:
                 gamma <= r <= s(pos)   if gamma <= s(pos)"""
         self.nodes += 1
 
-        # Depth <= 0 is QSearch. Here any position is searched as deeply as is needed for calmness, and so there is no reason to keep different depths in the transposition table.
+        # Depth <= 0 is Quiescence Search.
+        # Here any position is searched as deeply as is needed for calmness,
+        # and so there is no reason to keep different depths in the transposition table.
         depth = max(depth, 0)
 
         # Sunfish is a king-capture engine, so we should always check if we
@@ -288,19 +291,21 @@ class Searcher:
         # This allows us to define the moves, but only calculate them if needed.
         def moves():
             # First try not moving at all
-            if depth > 0 and not root and any(c in pos.board for c in 'RBNQ'):
-                yield None, -self.bound(pos.nullmove(), 1-gamma, depth-3, root=False)
-            # For QSearch we have a different kind of null-move
+            if depth > 0 and not root and any(c in pos.board for c in "RBNQ"):
+                yield None, -self.bound(pos.nullmove(), 1 - gamma, depth - 3, root=False)
+            # For Quiescence Search we have a different kind of null-move
             if depth == 0:
                 yield None, pos.score
-            # Then killer move. We search it twice, but the tp will fix things for us. Note, we don't have to check for legality, since we've already done it before. Also note that in QS the killer must be a capture, otherwise we will be non deterministic.
+            # Then killer move. We search it twice, but the tp will fix things for us.
+            # Note, we don't have to check for legality, since we've already done it before.
+            # Also note that in QS the killer must be a capture, otherwise we will be non deterministic.
             killer = self.tp_move.get(pos)
             if killer and (depth > 0 or pos.value(killer) >= QS_LIMIT):
-                yield killer, -self.bound(pos.move(killer), 1-gamma, depth-1, root=False)
+                yield killer, -self.bound(pos.move(killer), 1 - gamma, depth - 1, root=False)
             # Then all the other moves
             for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
                 if depth > 0 or pos.value(move) >= QS_LIMIT:
-                    yield move, -self.bound(pos.move(move), 1-gamma, depth-1, root=False)
+                    yield move, -self.bound(pos.move(move), 1 - gamma, depth - 1, root=False)
 
         # Run through the moves, shortcutting when possible
         best = -MATE_UPPER
@@ -348,10 +353,10 @@ class Searcher:
             self.depth = depth
             # The inner loop is a binary search on the score of the position.
             # Inv: lower <= score <= upper
-            # 'while lower != upper' would work, but play tests show a margin of 20 plays better.
+            # 'while lower != upper' would work, play tests show a margin of 20 plays better.
             lower, upper = -MATE_UPPER, MATE_UPPER
             while lower < upper - EVAL_ROUGHNESS:
-                gamma = (lower+upper+1)//2
+                gamma = (lower + upper + 1) // 2
                 score = self.bound(pos, gamma, depth)
                 if score >= gamma:
                     lower = score
@@ -389,26 +394,28 @@ if sys.version_info[0] == 2:
 
 
 def parse(c):
-    fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
-    return A1 + fil - 10*rank
+    fil, rank = ord(c[0]) - ord("a"), int(c[1]) - 1
+    return A1 + fil - 10 * rank
 
 
 def render(i):
     rank, fil = divmod(i - A1, 10)
-    return chr(fil + ord('a')) + str(-rank + 1)
+    return chr(fil + ord("a")) + str(-rank + 1)
 
 
 def print_pos(pos):
     print()
-    uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
-                  'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'}
+    uni_pieces = {
+        "R": "♜", "N": "♞", "B": "♝", "Q": "♛", "K": "♚", "P": "♟",
+        "r": "♖", "n": "♘", "b": "♗", "q": "♕", "k": "♔", "p": "♙", ".": "·",
+    }
     for i, row in enumerate(pos.board.split()):
-        print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
-    print('    a b c d e f g h \n\n')
+        print(" ", 8 - i, " ".join(uni_pieces.get(p, p) for p in row))
+    print("    a b c d e f g h \n\n")
 
 
 def main():
-    pos = Position(initial, 0, (True,True), (True,True), 0, 0)
+    pos = Position(initial, 0, (True, True), (True, True), 0, 0)
     searcher = Searcher()
     while True:
         print_pos(pos)
@@ -420,7 +427,7 @@ def main():
         # We query the user until she enters a (pseudo) legal move.
         move = None
         while move not in pos.gen_moves():
-            match = re.match('([a-h][1-8])'*2, input('Your move: '))
+            match = re.match("([a-h][1-8])" * 2, input("Your move: "))
             if match:
                 move = parse(match.group(1)), parse(match.group(2))
             else:
@@ -448,6 +455,6 @@ def main():
         pos = pos.move(move)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
