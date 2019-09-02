386f578efddd27864f1a9d3e5d81488150ed085f
Thomas Dybdahl Ahle <thomas@ahle.dk>
2019-09-02 15:24:55 +0200

    Added xboard options to allow tuning

--- xboard.py
+++ xboard.py
@@ -27,9 +27,16 @@ class Unbuffered(object):
     def write(self, data):
         self.stream.write(data)
         self.stream.flush()
+        sys.stderr.write(data)
+        sys.stderr.flush()
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
 sys.stdout = Unbuffered(sys.stdout)
+
+from datetime import datetime
+now = datetime.now()
+path = 'sunfish-' + now.strftime("%d:%m:%Y-%H:%M:%S:%f") + '.log'
+sys.stderr = open(path, 'a')
 
 def main():
     pos = tools.parseFEN(tools.FEN_INITIAL)
@@ -38,12 +45,15 @@ def main():
     color = WHITE
     our_time, opp_time = 1000, 1000 # time in centi-seconds
     show_thinking = False
+    options = {}
 
     stack = []
     while True:
         if stack:
             smove = stack.pop()
-        else: smove = input()
+        else:
+            smove = input()
+            print('>>>', smove, file=sys.stderr, flush=True)
 
         if smove == 'quit':
             break
@@ -56,10 +66,14 @@ def main():
             print('feature ping=1')
             print('feature sigint=0')
             print('feature variants="normal"')
+            print('feature option="qs_limit -spin 150 -100 1000"')
+            print('feature option="eval_roughness -spin 20 1 1000"')
             print('feature done=1')
 
         elif smove == 'new':
             stack.append('setboard ' + tools.FEN_INITIAL)
+            # Clear out the old searcher, including the tables
+            searcher = sunfish.Searcher()
 
         elif smove.startswith('setboard'):
             _, fen = smove.split(' ', 1)
@@ -69,6 +83,17 @@ def main():
         elif smove == 'force':
             forced = True
 
+        elif smove.startswith('option'):
+            _, aeqb = smove.split(maxsplit=1)
+            if '=' in aeqb:
+                name, val = aeqb.split('=')
+            else: name, val = aeqb, True
+            if name == 'qs_limit':
+                sunfish.QS_LIMIT = int(val)
+            if name == 'eval_roughness':
+                sunfish.EVAL_ROUGHNESS = int(val)
+            options[name] = val
+
         elif smove == 'go':
             forced = False
 
@@ -77,29 +102,36 @@ def main():
             # Let's follow the clock of our opponent
             if our_time >= 100 and opp_time >= 100:
                 use *= our_time/opp_time
-            
+
             start = time.time()
             for _ in searcher._search(pos):
+                ply = searcher.depth
+                entry = searcher.tp_score.get((pos, ply, True))
+                score = int(round((entry.lower + entry.upper)/2))
                 if show_thinking:
-                    ply = searcher.depth
-                    entry = searcher.tp_score.get((pos, ply, True))
-                    score = int(round((entry.lower + entry.upper)/2))
-                    dual_score = '{}:{}'.format(entry.lower, entry.upper)
                     used = int((time.time() - start)*100 + .5)
                     moves = tools.pv(searcher, pos, include_scores=False)
-                    print('{:>3} {:>8} {:>8} {:>13} \t{}'.format(
-                        ply, score, used, searcher.nodes, moves))
+                    seldepth = 0
+                    nps = int(searcher.nodes / (time.time() - start) + .5)
+                    print('{:>3} {:>8} {:>8} {:>13} {:>1} {:>4} \t{}'.format(
+                        ply, score, used, searcher.nodes, seldepth, nps, moves))
+                    print('# Hashfull: {:.3f}%; {} <= score < {}'.format(
+                        len(searcher.tp_score)/sunfish.TABLE_SIZE*100, entry.lower, entry.upper))
+                # If found mate, just stop
+                if entry.lower >= sunfish.MATE_UPPER:
+                    break
                 if time.time() - start > use/100:
                     break
             entry = searcher.tp_score.get((pos, searcher.depth, True))
             m, s = searcher.tp_move.get(pos), entry.lower
-            # We only resign once we are mated.. That's never?
+            # We sometimes make illegal moves when we're losing,
+            # so it's safer to just resign.
             if s == -sunfish.MATE_UPPER:
                 print('resign')
             else:
                 print('move', tools.mrender(pos, m))
-                pos = pos.move(m)
-                color = 1-color
+            pos = pos.move(m)
+            color = 1-color
 
         elif smove.startswith('ping'):
             _, N = smove.split()
@@ -131,7 +163,7 @@ def main():
         elif smove.startswith('nopost'):
             show_thinking = False
 
-        elif any(smove.startswith(x) for x in ('xboard','random','hard','accepted','level','easy','st')):
+        elif any(smove.startswith(x) for x in ('xboard','random','hard','accepted','level','easy','st','result','?')):
             print(f'# Ignoring command {smove}.')
 
         elif smove.startswith('reject'):
