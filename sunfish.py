26a49c377991090edab761e568259896b0f88fbe
Rutuparn <rutuparn.17U253@viit.ac.in>
2021-06-13 22:42:25 +0530

    Enter q to quit and h for help

--- sunfish.py
+++ sunfish.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import re, sys, time
+import re
+import sys
+import time
 from itertools import count
 from collections import namedtuple
 
@@ -10,60 +12,60 @@ from collections import namedtuple
 # Piece-Square tables. Tune these to change sunfish's behaviour
 ###############################################################################
 
-piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
+piece = {'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000}
 pst = {
-    'P': (   0,   0,   0,   0,   0,   0,   0,   0,
-            78,  83,  86,  73, 102,  82,  85,  90,
-             7,  29,  21,  44,  40,  31,  44,   7,
-           -17,  16,  -2,  15,  14,   0,  15, -13,
-           -26,   3,  10,   9,   6,   1,   0, -23,
-           -22,   9,   5, -11, -10,  -2,   3, -19,
-           -31,   8,  -7, -37, -36, -14,   3, -31,
-             0,   0,   0,   0,   0,   0,   0,   0),
-    'N': ( -66, -53, -75, -75, -10, -55, -58, -70,
-            -3,  -6, 100, -36,   4,  62,  -4, -14,
-            10,  67,   1,  74,  73,  27,  62,  -2,
-            24,  24,  45,  37,  33,  41,  25,  17,
-            -1,   5,  31,  21,  22,  35,   2,   0,
-           -18,  10,  13,  22,  18,  15,  11, -14,
-           -23, -15,   2,   0,   2,   0, -23, -20,
-           -74, -23, -26, -24, -19, -35, -22, -69),
-    'B': ( -59, -78, -82, -76, -23,-107, -37, -50,
-           -11,  20,  35, -42, -39,  31,   2, -22,
-            -9,  39, -32,  41,  52, -10,  28, -14,
-            25,  17,  20,  34,  26,  25,  15,  10,
-            13,  10,  17,  23,  17,  16,   0,   7,
-            14,  25,  24,  15,   8,  25,  20,  15,
-            19,  20,  11,   6,   7,   6,  20,  16,
-            -7,   2, -15, -12, -14, -15, -10, -10),
-    'R': (  35,  29,  33,   4,  37,  33,  56,  50,
-            55,  29,  56,  67,  55,  62,  34,  60,
-            19,  35,  28,  33,  45,  27,  25,  15,
-             0,   5,  16,  13,  18,  -4,  -9,  -6,
-           -28, -35, -16, -21, -13, -29, -46, -30,
-           -42, -28, -42, -25, -25, -35, -26, -46,
-           -53, -38, -31, -26, -29, -43, -44, -53,
-           -30, -24, -18,   5,  -2, -18, -31, -32),
-    'Q': (   6,   1,  -8,-104,  69,  24,  88,  26,
-            14,  32,  60, -10,  20,  76,  57,  24,
-            -2,  43,  32,  60,  72,  63,  43,   2,
-             1, -16,  22,  17,  25,  20, -13,  -6,
-           -14, -15,  -2,  -5,  -1, -10, -20, -22,
-           -30,  -6, -13, -11, -16, -11, -16, -27,
-           -36, -18,   0, -19, -15, -15, -21, -38,
-           -39, -30, -31, -13, -31, -36, -34, -42),
-    'K': (   4,  54,  47, -99, -99,  60,  83, -62,
-           -32,  10,  55,  56,  56,  55,  10,   3,
-           -62,  12, -57,  44, -67,  28,  37, -31,
-           -55,  50,  11,  -4, -19,  13,   0, -49,
-           -55, -43, -52, -28, -51, -47,  -8, -50,
-           -47, -42, -43, -79, -64, -32, -29, -32,
-            -4,   3, -14, -50, -57, -18,  13,   4,
-            17,  30,  -3, -14,   6,  -1,  40,  18),
+    'P': (0,   0,   0,   0,   0,   0,   0,   0,
+          78,  83,  86,  73, 102,  82,  85,  90,
+          7,  29,  21,  44,  40,  31,  44,   7,
+          -17,  16,  -2,  15,  14,   0,  15, -13,
+          -26,   3,  10,   9,   6,   1,   0, -23,
+          -22,   9,   5, -11, -10,  -2,   3, -19,
+          -31,   8,  -7, -37, -36, -14,   3, -31,
+          0,   0,   0,   0,   0,   0,   0,   0),
+    'N': (-66, -53, -75, -75, -10, -55, -58, -70,
+          -3,  -6, 100, -36,   4,  62,  -4, -14,
+          10,  67,   1,  74,  73,  27,  62,  -2,
+          24,  24,  45,  37,  33,  41,  25,  17,
+          -1,   5,  31,  21,  22,  35,   2,   0,
+          -18,  10,  13,  22,  18,  15,  11, -14,
+          -23, -15,   2,   0,   2,   0, -23, -20,
+          -74, -23, -26, -24, -19, -35, -22, -69),
+    'B': (-59, -78, -82, -76, -23, -107, -37, -50,
+          -11,  20,  35, -42, -39,  31,   2, -22,
+          -9,  39, -32,  41,  52, -10,  28, -14,
+          25,  17,  20,  34,  26,  25,  15,  10,
+          13,  10,  17,  23,  17,  16,   0,   7,
+          14,  25,  24,  15,   8,  25,  20,  15,
+          19,  20,  11,   6,   7,   6,  20,  16,
+          -7,   2, -15, -12, -14, -15, -10, -10),
+    'R': (35,  29,  33,   4,  37,  33,  56,  50,
+          55,  29,  56,  67,  55,  62,  34,  60,
+          19,  35,  28,  33,  45,  27,  25,  15,
+          0,   5,  16,  13,  18,  -4,  -9,  -6,
+          -28, -35, -16, -21, -13, -29, -46, -30,
+          -42, -28, -42, -25, -25, -35, -26, -46,
+          -53, -38, -31, -26, -29, -43, -44, -53,
+          -30, -24, -18,   5,  -2, -18, -31, -32),
+    'Q': (6,   1,  -8, -104,  69,  24,  88,  26,
+          14,  32,  60, -10,  20,  76,  57,  24,
+          -2,  43,  32,  60,  72,  63,  43,   2,
+          1, -16,  22,  17,  25,  20, -13,  -6,
+          -14, -15,  -2,  -5,  -1, -10, -20, -22,
+          -30,  -6, -13, -11, -16, -11, -16, -27,
+          -36, -18,   0, -19, -15, -15, -21, -38,
+          -39, -30, -31, -13, -31, -36, -34, -42),
+    'K': (4,  54,  47, -99, -99,  60,  83, -62,
+          -32,  10,  55,  56,  56,  55,  10,   3,
+          -62,  12, -57,  44, -67,  28,  37, -31,
+          -55,  50,  11,  -4, -19,  13,   0, -49,
+          -55, -43, -52, -28, -51, -47,  -8, -50,
+          -47, -42, -43, -79, -64, -32, -29, -32,
+          -4,   3, -14, -50, -57, -18,  13,   4,
+          17,  30,  -3, -14,   6,  -1,  40,  18),
 }
 # Pad tables and join piece and pst dictionaries
 for k, table in pst.items():
-    padrow = lambda row: (0,) + tuple(x+piece[k] for x in row) + (0,)
+    def padrow(row): return (0,) + tuple(x+piece[k] for x in row) + (0,)
     pst[k] = sum((padrow(table[i*8:i*8+8]) for i in range(8)), ())
     pst[k] = (0,)*20 + pst[k] + (0,)*20
 
@@ -75,16 +77,16 @@ for k, table in pst.items():
 # fast detection of moves that don't stay within the board.
 A1, H1, A8, H8 = 91, 98, 21, 28
 initial = (
-    '         \n'  #   0 -  9
-    '         \n'  #  10 - 19
-    ' rnbqkbnr\n'  #  20 - 29
-    ' pppppppp\n'  #  30 - 39
-    ' ........\n'  #  40 - 49
-    ' ........\n'  #  50 - 59
-    ' ........\n'  #  60 - 69
-    ' ........\n'  #  70 - 79
-    ' PPPPPPPP\n'  #  80 - 89
-    ' RNBQKBNR\n'  #  90 - 99
+    '         \n'  # 0 -  9
+    '         \n'  # 10 - 19
+    ' rnbqkbnr\n'  # 20 - 29
+    ' pppppppp\n'  # 30 - 39
+    ' ........\n'  # 40 - 49
+    ' ........\n'  # 50 - 59
+    ' ........\n'  # 60 - 69
+    ' ........\n'  # 70 - 79
+    ' PPPPPPPP\n'  # 80 - 89
+    ' RNBQKBNR\n'  # 90 - 99
     '         \n'  # 100 -109
     '         \n'  # 110 -119
 )
@@ -136,24 +138,32 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
         # as defined in the 'directions' map. The rays are broken e.g. by
         # captures or immediately in case of pieces such as knights.
         for i, p in enumerate(self.board):
-            if not p.isupper(): continue
+            if not p.isupper():
+                continue
             for d in directions[p]:
                 for j in count(i+d, d):
                     q = self.board[j]
                     # Stay inside the board, and off friendly pieces
-                    if q.isspace() or q.isupper(): break
+                    if q.isspace() or q.isupper():
+                        break
                     # Pawn move, double move and capture
-                    if p == 'P' and d in (N, N+N) and q != '.': break
-                    if p == 'P' and d == N+N and (i < A1+N or self.board[i+N] != '.'): break
+                    if p == 'P' and d in (N, N+N) and q != '.':
+                        break
+                    if p == 'P' and d == N+N and (i < A1+N or self.board[i+N] != '.'):
+                        break
                     if p == 'P' and d in (N+W, N+E) and q == '.' \
-                            and j not in (self.ep, self.kp, self.kp-1, self.kp+1): break
+                            and j not in (self.ep, self.kp, self.kp-1, self.kp+1):
+                        break
                     # Move it
                     yield (i, j)
                     # Stop crawlers from sliding, and sliding after captures
-                    if p in 'PNK' or q.islower(): break
+                    if p in 'PNK' or q.islower():
+                        break
                     # Castling, by sliding the rook next to the king
-                    if i == A1 and self.board[j+E] == 'K' and self.wc[0]: yield (j+E, j+W)
-                    if i == H1 and self.board[j+W] == 'K' and self.wc[1]: yield (j+W, j+E)
+                    if i == A1 and self.board[j+E] == 'K' and self.wc[0]:
+                        yield (j+E, j+W)
+                    if i == H1 and self.board[j+W] == 'K' and self.wc[1]:
+                        yield (j+W, j+E)
 
     def rotate(self):
         ''' Rotates the board, preserving enpassant '''
@@ -171,7 +181,7 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
     def move(self, move):
         i, j = move
         p, q = self.board[i], self.board[j]
-        put = lambda board, i, p: board[:i] + p + board[i+1:]
+        def put(board, i, p): return board[:i] + p + board[i+1:]
         # Copy variables and reset ep and kp
         board = self.board
         wc, bc, ep, kp = self.wc, self.bc, 0, 0
@@ -180,10 +190,14 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
         board = put(board, j, board[i])
         board = put(board, i, '.')
         # Castling rights, we move the rook or capture the opponent's
-        if i == A1: wc = (False, wc[1])
-        if i == H1: wc = (wc[0], False)
-        if j == A8: bc = (bc[0], False)
-        if j == H8: bc = (False, bc[1])
+        if i == A1:
+            wc = (False, wc[1])
+        if i == H1:
+            wc = (wc[0], False)
+        if j == A8:
+            bc = (bc[0], False)
+        if j == H8:
+            bc = (False, bc[1])
         # Castling
         if p == 'K':
             wc = (False, False)
@@ -229,8 +243,10 @@ class Position(namedtuple('Position', 'board score wc bc ep kp')):
 # Search logic
 ###############################################################################
 
+
 # lower <= s(pos) <= upper
 Entry = namedtuple('Entry', 'lower upper')
+
 
 class Searcher:
     def __init__(self):
@@ -271,7 +287,8 @@ class Searcher:
         # Look in the table if we have already searched this position before.
         # We also need to be sure, that the stored search was over the same
         # nodes as the current search.
-        entry = self.tp_score.get((pos, depth, root), Entry(-MATE_UPPER, MATE_UPPER))
+        entry = self.tp_score.get(
+            (pos, depth, root), Entry(-MATE_UPPER, MATE_UPPER))
         if entry.lower >= gamma and (not root or self.tp_move.get(pos) is not None):
             return entry.lower
         if entry.upper < gamma:
@@ -300,7 +317,7 @@ class Searcher:
                 yield killer, -self.bound(pos.move(killer), 1-gamma, depth-1, root=False)
             # Then all the other moves
             for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
-            #for val, move in sorted(((pos.value(move), move) for move in pos.gen_moves()), reverse=True):
+                # for val, move in sorted(((pos.value(move), move) for move in pos.gen_moves()), reverse=True):
                 # If depth == 0 we only try moves with high intrinsic score (captures and
                 # promotions). Otherwise we do all moves.
                 if depth > 0 or pos.value(move) >= QS_LIMIT:
@@ -312,7 +329,8 @@ class Searcher:
             best = max(best, score)
             if best >= gamma:
                 # Clear before setting, so we always have a value
-                if len(self.tp_move) > TABLE_SIZE: self.tp_move.clear()
+                if len(self.tp_move) > TABLE_SIZE:
+                    self.tp_move.clear()
                 # Save the move for pv construction and killer heuristic
                 self.tp_move[pos] = move
                 break
@@ -328,13 +346,15 @@ class Searcher:
         # but only if depth == 1, so that's probably fair enough.
         # (Btw, at depth 1 we can also mate without realizing.)
         if best < gamma and best < 0 and depth > 0:
-            is_dead = lambda pos: any(pos.value(m) >= MATE_LOWER for m in pos.gen_moves())
+            def is_dead(pos): return any(pos.value(m) >=
+                                         MATE_LOWER for m in pos.gen_moves())
             if all(is_dead(pos.move(m)) for m in pos.gen_moves()):
                 in_check = is_dead(pos.nullmove())
                 best = -MATE_UPPER if in_check else 0
 
         # Clear before setting, so we always have a value
-        if len(self.tp_score) > TABLE_SIZE: self.tp_score.clear()
+        if len(self.tp_score) > TABLE_SIZE:
+            self.tp_score.clear()
         # Table part 2
         if best >= gamma:
             self.tp_score[pos, depth, root] = Entry(best, entry.upper)
@@ -395,15 +415,19 @@ def render(i):
 
 def print_pos(pos):
     print()
-    uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
-                  'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'}
+    uni_pieces = {'R': '♜', 'N': '♞', 'B': '♝', 'Q': '♛', 'K': '♚', 'P': '♟',
+                  'r': '♖', 'n': '♘', 'b': '♗', 'q': '♕', 'k': '♔', 'p': '♙', '.': '·'}
     for i, row in enumerate(pos.board.split()):
         print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
     print('    a b c d e f g h \n\n')
 
 
 def main():
-    hist = [Position(initial, 0, (True,True), (True,True), 0, 0)]
+    print("♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖♙♙♙♙♙♙♙♙")
+    print("SUNFISH\n\n-> Enter h for help\n-> Enter q to quit")
+    print("♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖♙♙♙♙♙♙♙♙")
+
+    hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
     searcher = Searcher()
     while True:
         print_pos(hist[-1])
@@ -415,11 +439,22 @@ def main():
         # We query the user until she enters a (pseudo) legal move.
         move = None
         while move not in hist[-1].gen_moves():
-            match = re.match('([a-h][1-8])'*2, input('Your move: '))
+            usr_inpt = input('Your move: ')
+
+            if usr_inpt == 'q' or usr_inpt == "quit":
+                sys.exit()
+
+            if usr_inpt == 'h' or usr_inpt == "help":
+                print("\nHELP:")
+                print("Rows are annotated from bottom to top using numbers 1 to 8.")
+                print("Columns are annotated from left to right using alphabets a to h.")
+                print("If you wish to move your piece from column b row 2 to column b row 3, then enter the move as b2b3\n")
+                continue
+
+            match = re.match('([a-h][1-8])'*2, usr_inpt)
             if match:
                 move = parse(match.group(1)), parse(match.group(2))
             else:
-                # Inform the user when invalid input (e.g. "help") is entered
                 print("Please enter a move like g8f6")
         hist.append(hist[-1].move(move))
 
@@ -448,4 +483,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
