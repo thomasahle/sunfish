2c65a7e9166dd05a2e8a8eae11189e654731519c
tahle <ahle@fb.com>
2022-12-15 23:54:59 -0800

    First NNUE sunfish

---
+++ sunfish_nnue.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+path=/Users/ahle/repos/sunfish
+python3.10 -u $path/sunfish_nnue.py $path/models/$1
+
---
+++ test.sh
@@ -0,0 +1,18 @@
+~/repos/cutechess/build/cutechess-cli \
+   -engine conf=sunfish \
+   -each tc=3:0+2 arg=model_19.pickle \
+   -engine conf=sunfish-nnue name=er5 stderr=19.err option.EVAL_ROUGHNESS=5 \
+   -engine conf=sunfish-nnue name=er10 stderr=19.err option.EVAL_ROUGHNESS=10 \
+   -engine conf=sunfish-nnue name=er15 stderr=19.err option.EVAL_ROUGHNESS=15 \
+   -engine conf=sunfish-nnue name=er20 stderr=19.err option.EVAL_ROUGHNESS=20 \
+   -engine conf=sunfish-nnue name=er25 stderr=19.err option.EVAL_ROUGHNESS=25 \
+   -tournament round-robin \
+   -rounds 40 \
+   -games 1 \
+   -concurrency 16 \
+   -pgnout qs.pgn \
+   -ratinginterval 10 \
+   -outcomeinterval 10 \
+   -recover \
+   -openings file=gaviota-starters.pgn \
+   #-debug
