2773771d3b3027d0ea405e2f13cf8af08a86541b
tahle <ahle@fb.com>
2022-12-26 11:09:43 +0100

    build scripts

---
+++ build/pack.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "Usage: pack.sh engine.py model.pickle out.packed"
+
+#if [ -f $3 ]; then
+#    echo "$3 Already exists."
+#    exit 0
+#fi
+
+get_file_size() {
+   local file="$1"
+   local size=$(wc -c < "$file" | awk '{$1=$1};1')
+   echo "$size"
+}
+
+T=`mktemp`
+pyminify --rename-globals --remove-literal-statements "$1" > "$T"
+xz "$T"
+lt=$(get_file_size "$T.xz")
+echo "Length of script: $lt"
+
+lh=100
+head=""
+while [ $lh != ${#head} ]
+do
+   let lh=${#head}
+   head="""#!/bin/sh
+T=\`mktemp\`
+tail -c +$((lh+1)) "\$0"|xz -d>\$T
+(sleep 3;rm \$T)&python3 -u \$T
+exit
+"""
+   echo "Length of head: $lh"
+done
+
+printf "$head" > $2
+
+cat $T.xz >> $2
+rm $T.xz
+
+echo "Total length: $(get_file_size "$2")"
+
+chmod +x $2
