c6fe83e5db3c6b7980e7419fee4bd2b113841faf
tahle <ahle@fb.com>
2022-12-28 21:49:33 +0100

    updated null version to test

--- tools/quick_tests.sh
+++ tools/quick_tests.sh
@@ -9,11 +9,11 @@ $T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate1.fen --depth 2
 
 # Stockfish finds this at around depth 14 with normal search, but faster
 # if using "go mate". Currently it's too deep for sunfish to find.
-#$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/nullmove_mates.fen --depth 12
+# $T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/nullmove_mates.fen --depth 12
 
 # These mates should be findable at depth=4, but because of null-move
 # We need to go to depth=6.
-$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate2.fen --depth 7 --quick
+$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate2.fen --depth 4 --quick --limit 20
 
 $T "$1" ${2:-"--quiet"} draw $TOOLS/test_files/stalemate2.fen --depth 4
-echo "Should be about 73/130"
+echo "Should be about 85/130"
