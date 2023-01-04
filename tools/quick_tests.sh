b3e530200842c58c41101adbe10915b13b9ca473
tahle <ahle@fb.com>
2023-01-04 19:50:09 +0300

    cleaned up

--- tools/quick_tests.sh
+++ tools/quick_tests.sh
@@ -6,7 +6,8 @@ echo "$TOOLS"
 T="python3 $TOOLS/tester.py"
 
 echo "Mate in 1..."
-$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate1.fen --depth 2
+#$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate1.fen --depth 2
+$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate1.fen --movetime 10000
 echo
 
 # Stockfish finds this at around depth 14 with normal search, but faster
@@ -16,7 +17,12 @@ echo
 # These mates should be findable at depth=4, but because of null-move
 # We need to go to depth=6.
 echo "Mate in 2..."
-$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate2.fen --depth 4 --quick --limit 20
+#$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate2.fen --mate-depth 4 --limit 20
+$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate2.fen --movetime 10000 --limit 20
+echo
+
+echo "Mate in 3..."
+$T "$1" ${2:-"--quiet"} mate $TOOLS/test_files/mate3.fen --movetime 10000 --limit 5
 echo
 
 echo "Stalemate in 0..."
@@ -33,5 +39,5 @@ $T "$1" ${2:-"--quiet"} draw $TOOLS/test_files/stalemate2.fen --depth 4
 echo
 
 echo "Other puzzles..."
-$T "$1" ${2:-"--quiet"} best $TOOLS/test_files/win_at_chess_test.epd --ms 100
+$T "$1" ${2:-"--quiet"} best $TOOLS/test_files/win_at_chess_test.epd --movetime 100
 echo
---
+++ uci.py
@@ -0,0 +1,208 @@
+# Advanced UCI interface
+
+import re, time
+from tools import tools
+from concurrent.futures import ThreadPoolExecutor
+from threading import Event
+from functools import partial
+
+
+def render_move(move, white_pov):
+    if move is None:
+        return "(none)"
+    i, j = move.i, move.j
+    if not white_pov:
+        i, j = 119 - i, 119 - j
+    render = sunfish.render
+    return render(i) + render(j) + move.prom.lower()
+
+
+def parse_move(move_str, white_pov):
+    parse = sunfish.parse
+    i, j, prom = parse(move[:2]), parse(move[2:4]), move[4:].upper()
+    if not white_pov:
+        i, j = 119 - i, 119 - j
+    return Move(i, j, prom)
+
+
+def go_loop(searcher, hist, stop_event, max_movetime=0, max_depth=0, debug=False):
+    if debug:
+        print(f"Going movetime={max_movetime}, depth={max_depth}")
+
+    start = time.time()
+    best_move = None
+    for depth, move, score in searcher.search(hist):
+        # We never know when we've seen the last at a certain depth
+        # before we get to the next one
+        if depth - 1 >= max_depth:
+            break
+        best_move = render_move(move, white_pov=len(hist) % 2 == 1)
+        elapsed = time.time() - start
+        print(
+            "info depth", depth,
+            "score cp", score,
+            "time", round(1000 * elapsed),
+            "nodes", searcher.nodes,
+            "nps", round(searcher.nodes / elapsed),
+            # TODO: The PV may not actually be true, since it uses tp_move,
+            # in which higher depth moves may be overwritten by lower depth ones.
+            "pv", tools.pv(searcher, hist[-1], include_scores=False),
+        )
+        if best_move and elapsed > max_movetime * 2 / 3:
+            break
+        if stop_event.is_set():
+            break
+
+    print("bestmove", best_move)
+
+
+def mate_loop(searcher, hist, stop_event, max_movetime=0, max_depth=0, find_draw=False, debug=False):
+    start = time.time()
+    for d in range(int(max_depth) + 1):
+        if find_draw:
+            s0 = searcher.bound(hist[-1], 0, d)
+            elapsed = time.time() - start
+            print("info", "depth", d, "score lowerbound cp", s0)
+            s1 = searcher.bound(hist[-1], 1, d)
+            elapsed = time.time() - start
+            print("info", "depth", d, "score upperbound cp", s1)
+            if s0 >= 0 and s1 < 1:
+                break
+        else:
+            score = searcher.bound(hist[-1], sunfish.MATE_LOWER, d)
+            elapsed = time.time() - start
+            print(
+                "info depth", d,
+                "score cp", score,
+                "time", round(1000 * elapsed),
+                "pv", tools.pv(searcher, hist[-1], include_scores=False),
+            )
+            if score >= sunfish.MATE_LOWER:
+                break
+        if elapsed > max_movetime:
+            break
+        if stop_event.is_set():
+            break
+    move = searcher.tp_move.get(hist[-1])
+    move_str = render_move(move, white_pov=len(hist) % 2 == 1)
+    print("bestmove", move_str)
+
+
+def run(sunfish_module):
+    global sunfish
+    sunfish = sunfish_module
+
+    debug = False
+    hist = [sunfish.Position(sunfish.initial, 0, (True, True), (True, True), 0, 0)]
+    searcher = sunfish.Searcher()
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        # Noop future to get started
+        go_future = executor.submit(lambda: None)
+        do_stop_event = Event()
+
+        while True:
+            try:
+                args = input().split()
+                if not args:
+                    continue
+
+                elif args[0] == "stop":
+                    if go_future.running():
+                        if debug:
+                            print("Stopping go loop...")
+                        do_stop_event.set()
+                        go_future.result()
+                    else:
+                        if debug:
+                            print("Go loop not running...")
+
+                elif not go_future.done():
+                    print(f"Ignoring input {args}. Please call 'stop' first.")
+                    continue
+
+                # Make sure we are really done, and throw any errors that may have
+                # happened in the go loop.
+                go_future.result(timeout=0)
+
+                if args[0] == "uci":
+                    print("id name {sunfish.version}")
+                    for attr, (lo, hi) in sunfish.opt_ranges.items():
+                        default = getattr(sunfish, attr)
+                        print(f"option name {attr} type spin default {default} min {lo} max {hi}")
+                    print("uciok")
+
+                elif args[0] == "setoption":
+                    _, uci_key, _, uci_value = args[1:]
+                    setattr(sunfish, uci_key, int(uci_value))
+
+                elif args[0] == "isready":
+                    print("readyok")
+
+                elif args[0] == "quit":
+                    break
+
+                elif args[:2] == ["position", "startpos"]:
+                    del hist[1:]
+                    for ply, move in enumerate(args[3:]):
+                        hist.append(hist[-1].move(parse_move(move, ply % 2 == 0)))
+
+                elif args[:2] == ["position", "fen"]:
+                    fen = args[2:]
+                    board, color, castling, enpas, _hclock, _fclock = fen
+                    board = re.sub(r"\d", (lambda m: "." * int(m.group(0))), board)
+                    board = list(21 * " " + "  ".join(board.split("/")) + 21 * " ")
+                    board[9::10] = ["\n"] * 12
+                    board = "".join(board)
+                    wc = ("Q" in castling, "K" in castling)
+                    bc = ("k" in castling, "q" in castling)
+                    ep = sunfish.parse(enpas) if enpas != "-" else 0
+                    pos = sunfish.Position(board, 0, wc, bc, ep, 0)
+                    hist = [pos] if color == "w" else [pos, pos.rotate()]
+
+                elif args[0] == 'go':
+                    think = 10**6
+                    max_depth = 100
+                    loop = go_loop
+
+                    if args[1:] == [] or args[1] == "infinite":
+                        pass
+
+                    elif args[1] == "movetime":
+                        movetime = args[2]
+                        think = int(movetime) / 1000
+
+                    elif args[1] == "wtime":
+                        wtime, btime, winc, binc = [int(a) / 1000 for a in args[2::2]]
+                        # we always consider ourselves white, but uci doesn't
+                        if len(hist) % 2 == 0:
+                            wtime, winc = btime, binc
+                        think = min(wtime / 40 + winc, wtime / 2)
+                        # let's go fast for the first moves
+                        if len(hist) < 3:
+                            think = min(think, 1)
+
+                    elif args[1] == "depth":
+                        max_depth = int(args[2])
+
+                    elif args[1] in ("mate", "draw"):
+                        max_depth = int(args[2])
+                        loop = partial(mate_loop, find_draw=args[1]=="draw")
+
+                    do_stop_event.clear()
+                    go_future = executor.submit(loop,
+                        searcher, hist, do_stop_event, think, max_depth, debug=debug)
+
+                    # Make sure we get informed if the job fails
+                    def callback(fut):
+                        fut.result(timeout=0)
+                    go_future.add_done_callback(callback)
+
+            except KeyboardInterrupt:
+                if go_future.running():
+                    if debug:
+                        print("Stopping go loop...")
+                    do_stop_event.set()
+                    go_future.result()
+                break
+
